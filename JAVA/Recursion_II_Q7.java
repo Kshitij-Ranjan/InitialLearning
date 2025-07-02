0560bd116e3c1a13dcb6321855378467dc2eced9
KSHITIJ-RANJAN <kshitijranjan0512@gmail.com>
2025-07-02 18:27:31 +0530

    Recursion 2 Q7

--- JAVA/Recursion_II_Q7.java
+++ JAVA/Recursion_II_Q7.java
@@ -1,7 +1,23 @@
 package JAVA;
 
 public class Recursion_II_Q7 {
-    public static void main(String[] args){
 
+    public static void subSequence(String str, int indx, String newString){
+        if(indx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(indx);
+
+        //to be
+        subSequence(str, indx+1 , newString+currChar);
+        // not to be
+        subSequence(str, indx+1 , newString);
+    }
+
+    public static void main(String[] args){
+         String str = "abc";
+         subSequence(str, 0 , "");
     }
 }
