ebeaec13780df450d79583533c856a05f885af50
KSHITIJ-RANJAN <kshitijranjan0512@gmail.com>
2025-07-19 22:14:16 +0530

    Backtracking Q1

--- JAVA/Backtracking_Q1.java
+++ JAVA/Backtracking_Q1.java
@@ -1,17 +1,26 @@
 package JAVA;
 
 public class Backtracking_Q1 {
+    public static void newString(String str,int i, String newStr,String perm, int idx){
+        if(i == str.length()){
+            return;
+        }
+        char currChar = str.charAt(i);
+        newStr = str.substring(0, i) + str.substring(i + 1);
+        printPermutation(newStr, perm+currChar,idx+1);
+        newString(str,i+1,newStr, perm,idx);
+    }
     public static void printPermutation(String str, String perm, int idx) {
         if (str.isEmpty()) {
             System.out.println(perm);
             return;
         }
-
-        for (int i = 0; i < str.length(); i++) {
-            char currChar = str.charAt(i);
-            String newStr = str.substring(0, i) + str.substring(i + 1);
-            printPermutation(newStr, perm + currChar, idx + 1);
-        }
+        newString(str,0,"",perm,idx);
+//        for (int i = 0; i < str.length(); i++) {
+//            char currChar = str.charAt(i);
+//            String newStr = str.substring(0, i) + str.substring(i + 1);
+//            printPermutation(newStr, perm + currChar, idx + 1);
+//        }
     }
 
     public static void main(String args[]) {
