169e2acdfcb609bdce6cf0d68d836f6759d343e4
KSHITIJ RANJAN <kshitijranjan0512@gmail.com>
2025-08-27 10:38:10 +0530

    Delete VERILOG directory

--- VERILOG/ripple_carry_adder.v
+++
@@ -1,9 +0,0 @@
-module ripple_carry_adder(
-    output [3:0] sum,
-    output carry_out,
-    input [3:0] a,
-    input [3:0] b,
-    input carry_in
-);
-    assign {carry_out, sum} = a + b + carry_in;
-endmodule
